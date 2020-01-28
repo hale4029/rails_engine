64b3fd8d9f2a5d8a3e535444cb40aafd6b758503
Harrison Levin <harrison483@gmail.com>
2020-01-27 19:18:48 -0700

    model tests - merchants/items

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ApplicationRecord
-  validates_presence_of :first_name, :last_name
+  validates_presence_of :first_name,
+                        :last_name,
+                        :created_at,
+                        :updated_at
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
+RSpec.describe Merchant, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :first_name }
-    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :name }
     it { should validate_presence_of :created_at }
     it { should validate_presence_of :updated_at }
   end

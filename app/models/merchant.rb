3644ba953a77e8dc72650b66efdf5bcc7a75f045
Harrison Levin <harrison483@gmail.com>
2020-01-28 17:23:20 -0700

    find_all

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,21 +2,41 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   def self.find_merchant(params)
-    var = params[:id]
-    if var == "find"
-      if params.keys.include?('name')
-        self.where(name: params['name']).first
-      elsif params.keys.include?('merchant_id')
-        self.find(params['merchant_id'])
-      elsif params.keys.include?('created_at')
-        self.where(created_at: Time.zone.parse(params['created_at']).all_day).first
-      elsif params.keys.include?('updated_at')
-        self.where(updated_at: Time.zone.parse(params['updated_at']).all_day).first
-      else
-        "Error"
-      end
+    if params[:id] == "find"
+      self.find_query(params)
+    elsif params[:id] == "find_all"
+      self.find_all_query(params)
     else
       self.find(params['id'])
     end
   end
+
+  def self.find_query(params)
+    if params.keys.include?('name')
+      Merchant.where(name: params['name']).first
+    elsif params.keys.include?('merchant_id')
+      Merchant.find(params['merchant_id'])
+    elsif params.keys.include?('created_at')
+      Merchant.where(created_at: Time.zone.parse(params['created_at']).all_day).first
+    elsif params.keys.include?('updated_at')
+      Merchant.where(updated_at: Time.zone.parse(params['updated_at']).all_day).first
+    else
+      "Query param error"
+    end
+  end
+
+  def self.find_all_query(params)
+    if params.keys.include?('name')
+      Merchant.where(name: params['name'])
+    elsif params.keys.include?('merchant_id')
+      Merchant.where(id: params['merchant_id'])
+    elsif params.keys.include?('created_at')
+      Merchant.where(created_at: Time.zone.parse(params['created_at']).all_day)
+    elsif params.keys.include?('updated_at')
+      Merchant.where(updated_at: Time.zone.parse(params['updated_at']).all_day)
+    else
+      "Query param error"
+    end
+  end
+
 end
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -86,4 +86,73 @@ describe "Mechant API" do
     expect(merchant['attributes'].keys).to_not include('created_at')
     expect(merchant['attributes'].keys).to_not include('updated_at')
    end
-end
+
+   it "merchant find_all request: ID" do
+     merchant = create(:merchant)
+
+     get "/api/v1/merchants/find_all?merchant_id=#{merchant.id}"
+
+     expect(response).to be_successful
+
+     merchant = JSON.parse(response.body)['data']
+
+     expect(merchant.class).to eq(Array)
+     expect(merchant.length).to eq(1)
+
+     merchant = merchant.first
+     expect(merchant['attributes'].keys).to include('id')
+     expect(merchant['attributes'].keys).to include('name')
+     expect(merchant['attributes'].keys).to_not include('created_at')
+     expect(merchant['attributes'].keys).to_not include('updated_at')
+    end
+
+   it "merchant find_all request: NAME" do
+     merchant = create(:merchant, name: 'Harry')
+     merchant = create(:merchant, name: 'Harry')
+
+     get "/api/v1/merchants/find_all?name=#{merchant.name}"
+
+     expect(response).to be_successful
+
+     merchant = JSON.parse(response.body)['data']
+
+     expect(merchant.class).to eq(Array)
+     expect(merchant.length).to eq(2)
+
+     merchant = merchant.first
+     expect(merchant['attributes'].keys).to include('id')
+     expect(merchant['attributes'].keys).to include('name')
+     expect(merchant['attributes'].keys).to_not include('created_at')
+     expect(merchant['attributes'].keys).to_not include('updated_at')
+    end
+
+   it "merchant find_all request: CREATED AT" do
+     merchant = create(:merchant, created_at: (Time.now - 5.days))
+     create(:merchant, created_at: (Time.now - 5.days))
+     create(:merchant, created_at: (Time.now - 6.days))
+
+     get "/api/v1/merchants/find_all?created_at=#{merchant.created_at.to_s}"
+
+     expect(response).to be_successful
+
+     merchant = JSON.parse(response.body)['data']
+
+     expect(merchant.class).to eq(Array)
+     expect(merchant.length).to eq(2)
+    end
+
+   it "merchant find_all request: UPDATED AT" do
+     merchant = create(:merchant, updated_at: (Time.now - 5.days))
+     create(:merchant, updated_at: (Time.now - 5.days))
+     create(:merchant, updated_at: (Time.now - 6.days))
+
+     get "/api/v1/merchants/find_all?updated_at=#{merchant.updated_at.to_s}"
+
+     expect(response).to be_successful
+
+     merchant = JSON.parse(response.body)['data']
+
+     expect(merchant.class).to eq(Array)
+     expect(merchant.length).to eq(2)
+    end
+ end

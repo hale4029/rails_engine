42e889df7fed9eb1d809a75d7316a9d089489320
Harrison Levin <harrison483@gmail.com>
2020-01-30 15:24:01 -0700

    controller - index show

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -4,9 +4,19 @@ class Api::V1::InvoiceItemsController < ApplicationController
     render json: InvoiceItemSerializer.new(InvoiceItem.all)
   end
 
+  def show
+    render json: InvoiceItemSerializer.new(InvoiceItem.find_invoice_item(pass_params))
+  end
+
   # def index
   #   resource = InvoiceItem.find_resource(params)
   #   render json: InvoiceItemSerializer.new(resource.invoice_items)
   # end
+
+  private
+
+  def pass_params
+    params.permit(:id, :item_id, :created_at, :updated_at, :slug, :date, :invoice_id, :quantity, :unit_price)
+  end
 
 end

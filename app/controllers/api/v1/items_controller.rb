2f13823925313bbc531af7cb83ade55f22ccfcc8
Harrison Levin <harrison483@gmail.com>
2020-01-30 16:50:02 -0700

    controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,9 +4,18 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.all)
   end
 
+  def show
+    render json: ItemSerializer.new(Item.find_item(pass_params))
+  end
+
   # def index
   #   resource = Item.find_resource(params)
   #   render json: ItemSerializer.new(resource.items)
   # end
 
+  private
+
+  def pass_params
+    params.permit(:id, :name, :description, :created_at, :updated_at, :slug, :date, :merchant_id, :unit_price)
+  end
 end

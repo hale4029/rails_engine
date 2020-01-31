f8f2788fb5d891f143e76faa67844ad97b6a3c74
Harrison Levin <harrison483@gmail.com>
2020-01-30 20:38:36 -0700

    transaction serializer - cc

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,4 +1,10 @@
 class TransactionSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :invoice_id, :credit_card_number, :result
+
+
+  attribute :credit_card_number do |transactions|
+    transactions.credit_card_number.to_s
+  end
+
 end

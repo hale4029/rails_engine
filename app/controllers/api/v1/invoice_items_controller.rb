1ff1eceba57f573651da4b796a83362448cca3b7
Harrison Levin <harrison483@gmail.com>
2020-01-30 20:39:36 -0700

    :date remove -cntl

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -16,7 +16,7 @@ class Api::V1::InvoiceItemsController < ApplicationController
   private
 
   def pass_params
-    params.permit(:id, :item_id, :created_at, :updated_at, :slug, :date, :invoice_id, :quantity, :unit_price)
+    params.permit(:id, :item_id, :created_at, :updated_at, :slug, :invoice_id, :quantity, :unit_price)
   end
 
 end

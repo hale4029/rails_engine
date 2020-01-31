ef3e50eec7409fb2a32b6638c0304ad1cba8a793
Harrison Levin <harrison483@gmail.com>
2020-01-30 17:03:13 -0700

    controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,9 +4,8 @@ class Api::V1::TransactionsController < ApplicationController
     render json: TransactionSerializer.new(Transaction.all)
   end
 
-
   def show
-
+    render json: TransactionSerializer.new(Transaction.find_item(pass_params))
   end
 
   # def index
@@ -14,4 +13,9 @@ class Api::V1::TransactionsController < ApplicationController
   #   render json: TransactionSerializer.new(resource.transactions)
   # end
 
+  private
+
+  def pass_params
+    params.permit(:id, :invoice_id, :credit_card_number, :created_at, :updated_at, :slug, :date, :result)
+  end
 end

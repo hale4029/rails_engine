35a5b025dfe527b43e80479494afa24e5b590b64
Harrison Levin <harrison483@gmail.com>
2020-02-02 11:41:31 -0700

    controller

--- app/controllers/api/v1/merchant_solo_controller.rb
+++ app/controllers/api/v1/merchant_solo_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::MerchantSoloController < ApplicationController
 
-  # def service
-  #   resource = ResourceFinder.find_resource(params)
-  #   render json: MerchantSoloSerializer.new(resource.merchant)
-  # end
-
   def revenue
     merchant = Merchant.find(params[:merchant_slug])
     render json: merchant.revenue
@@ -13,12 +8,6 @@ class Api::V1::MerchantSoloController < ApplicationController
   def customer
     merchant = Merchant.find(params[:merchant_slug])
     render json: MerchantSoloSerializer.new(merchant.favorite_customer)
-  end
-
-  private
-
-  def pass_params
-    params.permit(:id, :name, :created_at, :updated_at, :slug, :date, :revenue, :most_revenue, :quantity)
   end
 
 end
